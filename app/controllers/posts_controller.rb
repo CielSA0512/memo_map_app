6f8a9b5d813efc0a8c16e09102b7a9059855dc1f
CielSA0512 <ktsune10@gmail.com>
2024-03-16 19:22:05 +0900

    単体テストコード（スポット投稿編）

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to root_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
     if post.update(post_params)
       redirect_to post_path(post.id)
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
