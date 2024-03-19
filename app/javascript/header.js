47fb289e244f4b15ff5d5f16d0653a29d50ff9f4
CielSA0512 <ktsune10@gmail.com>
2024-03-20 00:09:40 +0900

    index装飾

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -1,7 +1,16 @@
 function header (){
   const loginUser = document.getElementById("login_user");
   const optionBtn = document.getElementById("option_btn0");
-  const optionLogout = document.getElementById("option_btn1");
+  const userAccount = document.getElementById("option_btn1");
+  const userLogout = document.getElementById("option_btn2");
+  // アカウントネームにマウスを被せたときの挙動
+  loginUser.addEventListener("mouseover", function (){
+    this.setAttribute("style", "opacity: 0.5;");
+  });
+  loginUser.addEventListener("mouseout", function (){
+    this.removeAttribute("style");
+  });
+  // アカウントネームをクリックしたときの挙動 
   loginUser.addEventListener("click", function(){
     this.setAttribute("style", "opacity:0.5;");
     if (optionBtn.getAttribute("style") == "display:block;"){
@@ -9,15 +18,23 @@ function header (){
     } else{
       optionBtn.setAttribute("style", "display:block;")
     }
-    if (optionLogout.getAttribute("style") == "display:block;"){
-      optionLogout.removeAttribute("style");
-    } else{
-      optionLogout.setAttribute("style", "display:block;")
-    }
   })
   loginUser.addEventListener("click", function(){
     this.removeAttribute("style");
   })
+  // アカウントとログアウトにマウスを被せたときの挙動
+  userAccount.addEventListener("mouseover", function () {
+    this.setAttribute("style", "opacity: 0.5;");
+  });
+  userAccount.addEventListener("mouseout", function () {
+    this.removeAttribute("style");
+  });
+  userLogout.addEventListener("mouseover", function () {
+    this.setAttribute("style", "opacity: 0.5;");
+  });
+  userLogout.addEventListener("mouseout", function () {
+    this.removeAttribute("style");
+  });
 };
 
 window.addEventListener("turbo:load", header);
